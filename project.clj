77e2f72d6691e2bb61b0735a659a0283407fce5e
Johan Thoren <johan@thoren.xyz>
2021-09-18 21:38:03 +0200

    Version 0.5.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xyz.thoren/bibcal "0.5.8-SNAPSHOT"
+(defproject xyz.thoren/bibcal "0.5.8"
   :description (str "Calculate dates based on the Bible and the "
                     "1st Book of Enoch.")
   :url "https://github.com/johanthoren/bibcal"
